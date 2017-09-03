2bb529dfc511c57838e0c8103d5ca12e74dd235c
ManuCarmona <manucarcor@gmail.com>
2017-09-03 14:15:06 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -251,11 +251,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Misc dependency packages
 PRODUCT_PACKAGES += \
