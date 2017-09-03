e140164f9c7155a770b342a1fef9c69717cb84fe
manuelcarmona <manucarcor@gmail.com>
2017-09-03 10:12:07 +0200

    initial commit

---
+++ kernel/warnings-errors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# First patch
+cd ~/lineage
+DeleteAllWerror(){
+    local path="./"
+    if [ -n "$1" ];then
+        path="$1"
+    fi
+    local Makefiles=$(find $path -regex '.*\.mk$')
+    sed 's:[[:space:]]*-Werror=.*[^     ]$::' -i ${Makefiles[@]}
+    sed 's:-Werror[[:space:]]*::' -i ${Makefiles[@]}
+    sed 's:[[:space:]]*-Werror$::' -i ${Makefiles[@]}
+    sed 's:-Werror=.*[^     ][[:space:]]::' -i ${Makefiles[@]}
+}
+DeleteAllWerror $@
+
+# Second patch
+cd bionic
+DeleteAll__user(){
+    local Headers=$(find -regex '.*\.h')
+    sed 's:[[:space:]]__user[[:space:]]: :' ${Headers[@]} -i
+}
+DeleteAll__user
+
+DeleteAll__force(){
+    local Headers=$(find -regex '.*\.h')
+    sed 's:[[:space:]]__force[[:space:]]: :' ${Headers[@]} -i
+}
+DeleteAll__force
