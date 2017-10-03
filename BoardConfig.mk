1f716b33583ea8f6294ff267d4ec27f34cfdc39e
ManuCarmona <manucarcor@gmail.com>
2017-10-03 14:59:14 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,8 +60,8 @@ BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
-TARGET_KERNEL_CONFIG := msm_defconfig
+#TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+#TARGET_KERNEL_CONFIG := msm_defconfig
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
